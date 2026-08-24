8879fbebbf8f5e7f385504ccd394aeeed5717528
Nakscott <gninphanna@gmail.com>
2026-08-25 00:21:52 +0700

    Modified: Added Comment to the code.

--- Assignment/BasicCalculator.java
+++ Assignment/BasicCalculator.java
@@ -1,28 +1,54 @@
+/** Basic Calculator provide simple aritmetic operation:
+ * Add, Deduct, Multiply, Devide, Modulo
+ * The output will return as Total
+ */
 public class BasicCalculator {
+    /** Initate Total Varieble that will store the output */
         int total = 0;
 
+        /* get total */
         public int getTotal() {
             return  total;
         }
 
+        /* add x to total then return total  */
         public int add(int x) {
             total += x;
             return total;
         }
-
+        
+        /* deduct x to total then return total  */
         public int deduct(int x) {
             total -= x;
             return total;
         }
 
+        /* multiple the total with x then return total  */
         public int multiply(int x) {
             total *= x;
             return total;
         }
 
+        /* devide the total with x then return total  */
         public int divide(int x) {
+            /* check condition whether the number is not 0 */
             if (x != 0) {
                 total /= x;
+            }
+            /* if the number is 0 it will print out invalid number */
+            else {
+                System.out.println("Invalid Number Try again!");
+            }
+            return total;
+        }
+        
+        /**
+        * Computes total modulo x and returns the updated total.
+        * If x is 0, prints an error message and leaves total unchanged.
+         */
+        public int modulo(int x) {
+            if (x != 0) {
+                total %= x;
             }
             else {
                 System.out.println("Invalid Number Try again!");
@@ -30,11 +56,7 @@ public class BasicCalculator {
             return total;
         }
 
-        public int modulo(int x) {
-            total %= x;
-            return total;
-        }
-
+        /* Reset total to 0 */
         public void reset() {
             total = 0;
         }
